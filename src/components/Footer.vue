71f4e6c030b89073db0d5f6510c899feece8f704
Jeanine Schoessler <jeanine.mt@gmail.com>
2020-10-14 21:04:19 -0600

    Add layout and fontawesome icons Add navbar, footer and holygrail layout with icons

---
+++ src/components/Footer.vue
@@ -0,0 +1,33 @@
+<template>
+  <footer class="relative flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-gray-700 mt-3">
+    <div class="container px-4 mx-auto flex flex-wrap items-center justify-between">
+      <div class="w-full relative flex justify-between lg:w-auto  px-4 lg:static lg:block lg:justify-start">
+        <a class="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase text-white" href="/">
+          <font-awesome-icon :icon="logoIcon" /> AmazeTodos
+        </a>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faCheckSquare } from '@fortawesome/free-solid-svg-icons'
+export default {
+  name: "footer",
+  data() {
+    return {
+      logoIcon: faCheckSquare
+    }
+  },
+  methods: {
+    toggleNavbar: function(){
+      this.showMenu = !this.showMenu;
+    }
+  },
+
+  components: {
+    FontAwesomeIcon
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import { createApp, h } from 'vue'
 import App from './App.vue'
 import './assets/css/style.css'
-
+// import { library } from '@fortawesome/fontawesome-svg-core'
+// import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+// Vue.component('font-awesome-icon', FontAwesomeIcon)
 createApp(App).mount('#app')
 
 // new Vue({
